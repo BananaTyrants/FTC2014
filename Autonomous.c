4277c2cf0b2a9de1e750926cc59ce69ae51ced7d
Gautam <gautamprab@gmail.com>
2014-11-10 16:06:30 -0800

    Version 1.0.5, 11/10. Made functions, added new motors, prettied code

---
+++ Autonomous.c
@@ -0,0 +1,16 @@
+#include "JoystickDriver.c"
+
+void initializeRobot() {
+	return;
+}
+
+task main()
+{
+	initializeRobot();
+	waitForStart();
+
+	while (true) {
+		//do things that are autonomous
+
+	}
+}
